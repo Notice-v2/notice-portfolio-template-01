6f88a8ee88d718db06c44075342914dbc55ebcdd
Jonas Roussel <dev@jonas.bzh>
2024-04-18 17:22:42 +0200

    Update noisy background styling in globals.css and layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
 		<html lang="en">
 			<body>
 				<main className="max-w-[52rem] mx-auto px-4 pb-28 sm:px-6 md:px-8 xl:px-12 lg:max-w-6xl">{children}</main>
-				<div className="noisy-background"></div>
+				<div className="z-[-1] absolute w-full h-full min-h-screen opacity-25 invert-[1] bg-[url('/textured-noise.png')] top-0"></div>
 			</body>
 		</html>
 	)
